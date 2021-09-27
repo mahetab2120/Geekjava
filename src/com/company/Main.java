04b7e4d3db64e226b0276b272dc63bf5330411e2
mahetab2120 <80456414+mahetab2120@users.noreply.github.com>
2021-09-27 16:41:07 +0530

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -52,17 +52,27 @@ public class Main {
     }
     static void CheckPrime()
     {
-        int n=0;
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Enter number : ");
-        n=sc.nextInt();
-        if(n%2==0)
-        {
-            System.out.println(n+" is prime number.");
-        }
-        else{
-            System.out.println(n+" is not a prime number.");
+         int n,m,temp=0;
 
+        System.out.println("Enter number: ");
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        m=n/2;
+        if (n==0||n==1) {
+            System.out.println(n + " is not a prime Number.");
+        } else {
+            for (int i = 2; i < m; i++) {
+                if (n%i==0) 
+                {
+                     System.out.println(n + " is not a prime Number.");
+                     temp=1;
+                     break;
+                }
+            }
+            if(temp==0)  
+            {
+                 System.out.println(n+" is prime number"); 
+            }
         }
     }
     static void SumOfNumber()
